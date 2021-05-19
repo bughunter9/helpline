7c2b5f9d10b43ad0f2eede01d0e6899e2e9f7c16
MANROOP PARMAR <manroopsingh120@gmail.com>
2021-05-19 23:37:49 +0530

    Blockchain Setup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Header from './components/Header/Header';
 import Covid from './Covid/Covid';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
+import Resources from './components/Resources/Resources';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <About />
+      {/* <About /> */}
       {/* <Covid /> */}
+      <Resources />
       <Footer />
     </div>
   );
---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Resources.css";
+
+function Resources() {
+  return (
+    <div className="resources">
+      <h2>Hi</h2>
+    </div>
+  );
+}
+
+export default Resources;
