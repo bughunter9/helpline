4f87af542b42679c8011b753d80ced9f8ee64fe6
MANROOP PARMAR <manroopsingh120@gmail.com>
2021-07-06 18:56:14 +0530

    Portis ID Updated

--- src/web3.js
+++ src/web3.js
@@ -2,7 +2,7 @@ import Portis from "@portis/web3";
 import Web3 from "web3";
 
 const portis = new Portis(
-  "71ae6adb-ac26-46f8-897f-933cbaf98389",
+  "a829b9e7-1449-4dd1-8302-bed21b9d8e53",
   "maticMumbai"
 );
 const web3 = new Web3(portis.provider, "https://rpc-mumbai.matic.today");
